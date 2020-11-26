11be2dd17ac2bf2251d703465601130ba1a89986
Pedro Santana <pedro.gustavo.santana@outlook.com>
2020-11-26 10:50:23 -0300

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pedro/roboica_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/pedro/roboica_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/pedro/roboica_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/pedro/roboica_ws/build'
+export PYTHONPATH="/home/pedro/roboica_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/pedro/roboica_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/pedro/roboica_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/husky/husky_base/CMakeFiles/horizon_legacy.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Logger.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Logger.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Message.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Message.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Message_cmd.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Message_cmd.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Message_data.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Message_data.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Message_request.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Message_request.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Number.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Number.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/Transport.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/Transport.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/crc.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/crc.cpp.o"
+  "/home/pedro/roboica_ws/src/husky/husky_base/src/horizon_legacy/linux_serial.cpp" "/home/pedro/roboica_ws/build/husky/husky_base/CMakeFiles/horizon_legacy.dir/src/horizon_legacy/linux_serial.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"husky_base\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pedro/roboica_ws/src/husky/husky_base/include"
+  "/home/pedro/roboica_ws/devel/include"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
